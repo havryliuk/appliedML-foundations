ce80246bde682d9b2afad9b7a64ecfaef78e3cf9
Oleksandr Gavryliuk <oleksander.havryliuk@gmail.com>
2022-07-11 17:50:40 +0300

    Create simple test for workflow

---
+++ 1/example.py
@@ -0,0 +1,17 @@
+from pandas import DataFrame
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from warnings import simplefilter
+from sklearn.exceptions import ConvergenceWarning
+
+simplefilter('ignore', category=ConvergenceWarning)
+
+
+def generate_iris_predictions():
+    iris = load_iris()
+    iris_dataframe = DataFrame(load_iris().data)
+    print(iris_dataframe.head())
+    print(iris_dataframe.describe())
+
+    model = LogisticRegression().fit(iris['data'], iris['target'])
+    return model.predict(iris['data'])
